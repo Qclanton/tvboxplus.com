399939427dd613359c88e6db783ac13f1fe8fed3
Anton Qcl <qcl.anton@gmail.com>
2015-09-18 19:49:49 -0400

    Added address autocomplete
    Added address autocomplete and definition of the longitude and latitude.

--- wp-content/plugins/coverage_map/Views/Options.php
+++ wp-content/plugins/coverage_map/Views/Options.php
@@ -7,6 +7,7 @@
 <form method="POST" action="">
     <input type="hidden" name="action" value="set"/>
     
+    <h2>Center</h2>
     <table class="form-table">
         <tbody>
             <tr>
@@ -14,13 +15,38 @@
                     <label for="options[center][address]">Address</label>
                 </th>
                 <td>
-                    <input name="options[center][address]" type="text" value="<?= $center->address ?>" class="regular-text">
+                    <input id="center-address" name="options[center][address]" type="text" value="<?= $center->address ?>" class="regular-text">
+                    <input id="center-longitude" name="options[center][longitude]" type="hidden" value="<?= $center->longitude ?>">
+                    <input id="center-latitude" name="options[center][latitude]" type="hidden" value="<?= $center->latitude ?>">
                 </td>
             </tr>
         <tbody>
     </table>
     
+    <h2>Points</h2>
+    <p>There is no points yet</p>
+    
     <div class="tablenav bottom">
 		<button class="button button-primary">Save</button>
 	</div>
 </form>
+
+
+
+<!-- Address autocomplete -->
+<script>
+function autocompleteAddress() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('center-address'),
+        {types: ['geocode']}
+    );
+
+    autocomplete.addListener('place_changed', function() {
+        var place = this.getPlace();
+        
+        document.getElementById('center-longitude').setAttribute('value', place.geometry.location.lng());
+        document.getElementById('center-latitude').setAttribute('value', place.geometry.location.lat());
+    });
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=places&callback=autocompleteAddress" async defer></script>
